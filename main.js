c9d732d3119f2b244bda55afaa356d6e5be88df9
June <lovekwon200@gmail.com>
2020-08-23 15:15:52 +0900

    then;

--- main.js
+++ main.js
@@ -6,40 +6,79 @@ var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pName=url.parse(_url, true).pathname;
-    var title=queryData.id;
+    
 
     if(pName==='/'){
-      fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
+      //웰컴+123
+      
+      
+      if(queryData.id===undefined){ //웰컴페이지
+        
+        fs.readdir('./data',function(error,fileList){
+          console.log(fileList);
+        })
+          var title='Welcome';
+          var description='Hello Node.js';
+          var template=`
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>
+            ${description}
+            </p>
+            </body>
+            </html>
+    
+            `
+          response.writeHead(200);
+          response.end(template);
+      }else{
+        fs.readFile(`data/${queryData.id}`,'utf8',function(err,description){
 
-        var template=`
-          <!doctype html>
-          <html>
-          <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-          </head>
-          <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>
-          ${description}
-          </p>
-          </body>
-          </html>
-  
-          `
-        response.writeHead(200);
-        response.end(template);
-      })
+          var title=queryData.id;
+          var template=`
+            <!doctype html>
+            <html>
+            <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+            </head>
+            <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                <li><a href="/?id=HTML">HTML</a></li>
+                <li><a href="/?id=CSS">CSS</a></li>
+                <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>
+            ${description}
+            </p>
+            </body>
+            </html>
+    
+            `
+          response.writeHead(200);
+          response.end(template);
+        })
+
+      }
+      
 
     }else{
       response.writeHead(404);
-        response.end('Not Found');
+      response.end('Not Found');
     }
     
     
